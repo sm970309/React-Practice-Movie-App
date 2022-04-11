52e41d5b4aac29cd37097f1c51875bf6657dab16
sm970309 <sm970309@gmail.com>
2022-04-11 16:10:48 +0900

    Switch 삭제 -> Routes 사용

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import Home from './routes/Home'
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link
+} from "react-router-dom";
 function App() {
-    return(
-        <Home/>
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+            </Routes>
+        </Router>
+
     )
 }
 export default App;
